5cbdfd0a13df3e6cd03be380c431ab8e9648234c
Adeolu0707 <adepojujolaadeolu@gmail.com>
2023-03-22 08:42:30 -0700

    updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,18 @@
-/*
- * File: 1-alphabet.c
- * Auth: Mojolaoluwa Adepoju 
- */
-
 #include "main.h"
-
 /**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
- */
+ *  * print_alphabet - prints the english alphabet from a-z.
+ *   * Return: Nothing.
+ *    */
 void print_alphabet(void)
 {
-    char letter;
+	    char c;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-        _putchar(letter);
-
-    _putchar('\n');
+	        for (c = 'a'; c <= 'z'; c++)
+			    {
+				            _putchar(c);
+					        }
+		    _putchar('\n');
 }
+
+
 
